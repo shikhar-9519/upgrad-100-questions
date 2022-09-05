eefbe3fd3876a3e7eb492f9d40ddf11e0923dfea
Shikhar Gupta <87434089+shikhar-9519@users.noreply.github.com>
2022-09-05 17:30:29 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,23 @@
+
+package college;
+
+import java.util.Map;
+import java.util.Scanner;
+
+class first {
+    public static void main(String[] args) {
+            srm s=new srm();
+            rec r=new rec();
+            Scanner sc=new Scanner(System.in);
+            int opt=0;
+            do{
+                menu();
+                opt=sc.nextInt();
+                switch(opt)
+                {
+                    case 1:
+                        
+                }
+            }
+    }
+}
